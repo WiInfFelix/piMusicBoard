fb4d9133513f2d13d3d7207639a29bb14a1b23e2
Felix <schefflerfelix95@gmail.com>
2020-07-19 15:37:02 +0200

    Made buttons work + created stlysheet to make it look not like crap

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	r := gin.Default()
 
-	r.LoadHTMLGlob("templates/*")
+	r.LoadHTMLGlob("templates/*.html")
+	r.Static("/css", "./templates/css")
 
 	r.GET("/", func(context *gin.Context) {
 		context.HTML(http.StatusOK, "index.html", gin.H{})
@@ -19,12 +20,10 @@ func main() {
 	r.POST("/volume/:vol", SetVolume)
 	r.POST("/video/:videoUrl", PlayYoutubeVideo)
 
-
 	r.Run(":8080")
 }
 
-
-func SetVolume(c *gin.Context){
+func SetVolume(c *gin.Context) {
 	volume := c.Param("vol")
 
 	cmd := fmt.Sprintf("amixer -q -M sset %s%", volume)
@@ -32,13 +31,12 @@ func SetVolume(c *gin.Context){
 	volCmd := exec.Command("Set Loudness", cmd)
 	fmt.Printf("LOG: Trying to set volume to %s%", volume)
 	err := volCmd.Run()
-	if err != nil{
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"msg": err})
 	} else {
 		c.JSON(http.StatusOK, gin.H{"msg": "exec successful"})
 	}
 }
-
 
 func PlayYoutubeVideo(c *gin.Context) {
 	videoUrl := c.Param("videoUrl")
